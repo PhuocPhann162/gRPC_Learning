b2dca5e0e2e506a016b70c5d47224ba44a24e33e
Phuoc Phan <21520405@gm.uit.edu.vn>
2024-06-15 23:27:46 +0700

    initialize gRPC project

---
+++ GrpcGreeterClient/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Net.Client;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GrpcGreeterClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSendMessages_Click(object sender, RoutedEventArgs e)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:7201");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+
+            txtMessageReceive.Text = "Greeting: " + reply.Message;
+
+        }
+    }
+}
